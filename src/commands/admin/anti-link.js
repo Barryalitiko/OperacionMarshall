eb66844329b3d6431bab69a10d419667040fb578
Barryalitiko <tiobarryx@gmail.com>
2024-11-22 20:00:53 +0100

    Delete src/commands directory

--- src/commands/admin/anti-link.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "anti-legios",
-  description: "Prueba básica del comando anti-legios",
-  commands: ["anti-legio", "antilegios"],
-  handle: async ({ args, sendReply }) => {
-    await sendReply(`¡El comando fue recibido! Argumentos: ${args}`);
-  },
-};
